273b684e3f4157db1e299b0452c2aefab9360b23
Rishat Zakirov <rzakirovt@gmail.com>
2022-02-22 10:08:48 +0700

    File creation sample

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::{
+    fs::{File, OpenOptions},
+    io::{Seek, Write},
+    path::Path, os::unix::prelude::FileExt,
+};
+
 fn main() {
     println!("Hello, world!");
+
+    let file_name = "hello.db";
+    let path = Path::new(file_name);
+    let display = path.display();
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(true)
+        .open(path)
+        .unwrap();
+
+    let page_size = 4096;
+
+    let mut buf: &[u8] = &[0, 0, 0, 1, 1];
+    file.write_at(buf, 0);
 }
+
+struct Pointer {
+    right: u32,
+}
+
+struct Page {}
+
+impl Page {}
