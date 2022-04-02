01ce2d89cb453b18475974fc5338651f50a7b23d
Rishat Zakirov <rzakirovt@gmail.com>
2022-04-02 20:20:21 +0400

    Started page de/serialization

--- README.md
+++ README.md
@@ -3,17 +3,17 @@ Paging study project
 
 Pager is for columnar store database, so it is a bit different from regular row databases.
 
-Size of page - 4096 bytes. First 8 bytes allocated for header.
+Size of page - 4096 bytes. First 12 bytes allocated for header.
 
 Leaf page header structure:
 
 |Offset|Size|Description|
 |---|---|---|
 |0  |1  |Page type|
-|1  |3  |Column Id|
-|4  |4  |Reserved |
+|1  |4  |Column Id|
+|5  |7  |Reserved |
 
-Last 4096 - 8 = 4088 is for body (payload).
+Last 4096 - 12 = 4084 is for body (payload).
 Body has two types of data: row local id and column data. Row identifiers fills body from start to end while column data from end to start.
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::{
     fs::{File, OpenOptions},
     io::{Seek, Write},
-    path::Path, os::unix::prelude::FileExt,
+    os::unix::prelude::FileExt,
+    path::Path,
 };
+
+const PAGE_SIZE: usize = 4096;
 
 fn main() {
     println!("Hello, world!");
@@ -25,10 +28,79 @@ fn main() {
     file.write_at(buf, 0);
 }
 
+fn serialize_page(page: Page) -> Vec<u8> {
+    let mut buf: Vec<u8> = vec![0; PAGE_SIZE];
+    buf[0] = page.header.page_type as u8;
+
+    let column_id_in_bytes = page.header.column_id.to_le_bytes();
+    buf[1] = column_id_in_bytes[0];
+    buf[2] = column_id_in_bytes[1];
+    buf[3] = column_id_in_bytes[2];
+    buf[4] = column_id_in_bytes[3];
+
+    buf
+}
+
+fn deserialize_to_page(buffer: Vec<u8>) -> Page {
+    let vec = &buffer[1..5];
+    let column_id_bytes = <&[u8; 4]>::try_from(vec).unwrap();
+
+    Page {
+        header: PageHeader {
+            page_type: PageType::from(buffer[0]),
+            column_id: u32::from_le_bytes(*column_id_bytes),
+        },
+    }
+}
+
 struct Pointer {
     right: u32,
 }
 
-struct Page {}
+struct Page {
+    header: PageHeader,
+}
+
+struct PageHeader {
+    page_type: PageType,
+    column_id: u32,
+}
+
+#[derive(Copy, Clone, PartialEq, PartialOrd, Debug)]
+
+enum PageType {
+    Index = 0,
+    Leaf = 1,
+}
+
+impl PageType {
+    fn from(b: u8) -> PageType {
+        match b {
+            0 => PageType::Index,
+            1 => PageType::Leaf,
+            _ => panic!("Invalid page type")
+        }
+    }
+}
 
 impl Page {}
+
+#[cfg(test)]
+mod tests {
+    use crate::{deserialize_to_page, serialize_page, Page, PageHeader, PageType};
+
+    #[test]
+    fn serialization_desiralization_check() {
+        let bytes = serialize_page(Page {
+            header: PageHeader {
+                page_type: PageType::Leaf,
+                column_id: u32::MAX,
+            },
+        });
+
+        let page = deserialize_to_page(bytes);
+
+        assert_eq!(page.header.column_id, u32::MAX);
+        matches!(page.header.page_type, PageType::Leaf);
+    }
+}
