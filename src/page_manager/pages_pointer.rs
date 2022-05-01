37d24ac381df20ee58b862a84420a8c53a05fe89
Rishat Zakirov <rzakirovt@gmail.com>
2022-05-01 11:35:07 +0400

    pages_pointer: refactorings

--- src/page_manager/pages_pointer.rs
+++ src/page_manager/pages_pointer.rs
@@ -15,44 +15,60 @@ impl PagesPointer for PagesPointerImpl {
         struct_local_pages.copy_from_slice(&structure_pages.value);
         struct_local_pages.sort();
 
-        if s_pages_len > 0 {
-            if struct_local_pages.last().copied().unwrap() > file_pages - 1 {
-                return get_free_page_identifiers(file_pages, content_chunks_len);
-            }
-
-            if s_pages_len == content_chunks_len {
-                return structure_pages.value.to_vec();
-            }
-
-            if s_pages_len < content_chunks_len {
-                let mut pages: Vec<u64> = Vec::new();
-
-                let mut free_pages = get_free_page_identifiers(
-                    file_pages,
-                    content_chunks_len - struct_local_pages.len(),
-                );
-                pages.append(&mut struct_local_pages.clone());
-                pages.append(&mut free_pages);
-
-                return pages;
-            }
+        if file_pages > 0
+            && s_pages_len > 0
+            && struct_local_pages.last().copied().unwrap() > file_pages - 1
+        {
+            panic!("structure_pages have id that can not be placed in file");
         }
 
-        if struct_local_pages.len() > content_chunks_len {
-            return struct_local_pages.to_vec();
+        let mut pages: Vec<u64> = Vec::new();
+
+        if s_pages_len > content_chunks_len {
+            return struct_local_pages;
         }
 
-        get_free_page_identifiers(file_pages, content_chunks_len)
+        let mut free_pages = get_free_page_identifiers(
+            file_pages,
+            content_chunks_len - struct_local_pages.len(),
+            struct_local_pages.clone(),
+        );
+        pages.append(&mut struct_local_pages.clone());
+        pages.append(&mut free_pages);
+        pages.sort();
+
+        return pages;
     }
 }
 
-fn get_free_page_identifiers(file_pages: u64, length: usize) -> Vec<u64> {
+/// Returns a person with the name given them
+///
+/// # Arguments
+///
+/// * `start_from` - inclusive
+fn get_free_page_identifiers(start_from: u64, length: usize, existing_pages: Vec<u64>) -> Vec<u64> {
     let mut result = vec![];
-    for id in file_pages..(length as u64 + file_pages) as u64 {
-        result.push(id);
+
+    if length == 0 {
+        return result;
     }
 
-    result
+    let mut start = start_from;
+    loop {
+        if existing_pages.contains(&start) {
+            start += 1;
+
+            continue;
+        }
+
+        result.push(start);
+
+        if result.len() == length {
+            return result;
+        }
+
+        start += 1;
+    }
 }
 
 #[cfg(test)]
@@ -90,14 +106,14 @@ mod tests {
             PAGE_SIZE as u64 * 6,
             3,
             &StructurePages {
-                value: vec![8, 7, 6],
+                value: vec![3, 4, 2],
             },
         );
 
         assert_eq![3, ids.len()];
-        assert_eq![6, ids[0]];
-        assert_eq![7, ids[1]];
-        assert_eq![8, ids[2]];
+        assert_eq![2, ids[0]];
+        assert_eq![3, ids[1]];
+        assert_eq![4, ids[2]];
     }
 
     #[test]
@@ -133,24 +149,6 @@ mod tests {
         assert_eq![6, ids[3]];
     }
 
-    #[test]
-    fn get_identifiers_when_structure_page_id_more_than_file_size_pages_return_free_pages() {
-        // this means that something went wrong that was not specified.
-        // it can be panic for bug catching reasons, but more appropriate to be able to continue in working order
-        let ids = PagesPointerImpl::get_identifiers(
-            PAGE_SIZE as u64 * 4,
-            4,
-            &StructurePages {
-                value: vec![2, 4, 5, 6],
-            },
-        );
-
-        assert_eq![4, ids.len()];
-        assert_eq![4, ids[0]];
-        assert_eq![5, ids[1]];
-        assert_eq![6, ids[2]];
-        assert_eq![7, ids[3]];
-    }
 
     #[test]
     fn get_identifiers_when_content_chunks_len_zero_structure_pages_len_zero_return_empty() {
@@ -178,5 +176,44 @@ mod tests {
         assert_eq![1, ids[0]];
         assert_eq![2, ids[1]];
         assert_eq![3, ids[2]];
+    }
+
+    #[test]
+    fn get_identifiers_when_file_size_zero_return_structure_and_free_pages() {
+        let ids = PagesPointerImpl::get_identifiers(0, 2, &StructurePages { value: vec![0, 1, 2] });
+
+        assert_eq![3, ids.len()];
+        assert_eq![0, ids[0]];
+        assert_eq![1, ids[1]];
+        assert_eq![2, ids[2]];
+    }
+
+    #[test]
+    fn get_identifiers_when_structure_pages_has_some_of_existing_return_mix_of_structure_and_free_pages(
+    ) {
+        let ids = PagesPointerImpl::get_identifiers(
+            PAGE_SIZE as u64 * 4,
+            5,
+            &StructurePages { value: vec![1, 3] },
+        );
+
+        assert_eq![5, ids.len()];
+        assert_eq![1, ids[0]];
+        assert_eq![3, ids[1]];
+        assert_eq![4, ids[2]];
+        assert_eq![5, ids[3]];
+        assert_eq![6, ids[4]];
+    }
+
+    #[test]
+    #[should_panic]
+    fn get_identifiers_when_structure_pages_contains_not_existing_no_zero_page_panic() {
+        PagesPointerImpl::get_identifiers(
+            PAGE_SIZE as u64 * 4,
+            5,
+            &StructurePages {
+                value: vec![1, 3, 4],
+            }, // id 4 cannot be exists, because
+        );
     }
 }
